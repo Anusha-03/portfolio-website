6de38f7ebbbcae9afd2e20ed7ecf57f6bac059b1
Anusha Dixit <anushadixit03@gmail.com>
2021-11-19 14:31:42 +0530

    Adding blog to portfolio website

---
+++ Blog/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const articleRouter = require("./routes/articles");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use("/articles", articleRouter);
+
+app.get("/", (req, res) => {
+    const articles = [{
+        title: "Test Article",
+        createDate: Date.now(),
+        description: "Test description"
+    },
+    {
+        title: "Test Article 2",
+        createDate: Date.now(),
+        description: "Test description 2"
+    }];
+    res.render("index", { text: 'Hello'});
+});
+app.listen(5000);
