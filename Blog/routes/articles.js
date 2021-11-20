bbd6c65ff4931de208b7098245219709f478b030
Anusha Dixit <anushadixit03@gmail.com>
2021-11-20 21:46:35 +0530

    Updating Blog in portfolio website

--- Blog/routes/articles.js
+++ Blog/routes/articles.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const Article = require('./../models/article');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("In articles");
+router.get("/new", (req, res) => {
+    res.render('articles/new');
+});
+
+router.get('/:id', (req, res) => {
+        
+});
+
+router.post('/', (req, res) => {
+    const article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    });
+    try{
+    (async function() {
+        await article.save();
+    })();
+    
+res.rediect(`/articles/${article.id}`); }
+    catch(e) {
+       res.render('article/new', {article: article});
+    }
 });
 
 module.exports = router;
